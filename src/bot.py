ae05cbbe43dafda23c87d5f9abfddfbfc1b1d2c1
teller308 <teller308@gmail.com>
2019-08-19 01:17:57 +0400

    initial commit. make request and get result

---
+++ src/bot.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+with open('config.json') as config:
+    json_dict = json.load(config)
+
+
+_TOKEN = json_dict['token']
+_BASE_URL = 'https://api.telegram.org/bot' + _TOKEN
+_METHOD = '/getUpdates'
+
+with open('result.json', 'w') as jfile:
+    json_resp = requests.get(_BASE_URL + _METHOD)
+    json.dump(json_resp.json(), jfile)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
