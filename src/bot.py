c27080b229e6a32ea98a0682ae9e2e651d51a1b1
Johny Teller <teller308@gmail.com>
2019-08-20 18:33:54 +0400

    can manage webhook, but can't recieve updates

--- src/bot.py
+++ src/bot.py
@@ -1,23 +1,54 @@
 import json
 
 import requests
-from flask import Flask
+from flask import Flask, request
+
 
 app = Flask(__name__)
 
 
-with open('config.json') as config:
+with open('private/config.json') as config:
     json_dict = json.load(config)
 
 
 _TOKEN = json_dict['token']
-_BASE_URL = 'https://api.telegram.org/bot' + _TOKEN
-_METHOD = '/getUpdates'
+_DOMAIN = json_dict['domain']
 
-with open('result.json', 'w') as jfile:
-    json_resp = requests.get(_BASE_URL + _METHOD)
-    json.dump(json_resp.json(), jfile)
+_BASE_URL = 'https://api.telegram.org/bot' + _TOKEN
+
+_GET_UPDATES = '/getUpdates'
+
+_SET_WEBHOOK = '/setWebhook'
+_DELETE_WEBHOOK = '/deleteWebhook'
+_GET_WEBHOOK_INFO = '/getWebhookInfo'
+
+_ALLOWED_WEBHOOK = '/webhook' + _TOKEN
+
+
+def setWebhook():
+    with open('private/keeper_wh_public.pem') as certfile:
+        resp = requests.get(_BASE_URL + _GET_WEBHOOK_INFO)
+        print(resp.text)
+        resp = requests.get(_BASE_URL + _DELETE_WEBHOOK)
+        print(resp.text)
+
+        webhook_data = {'url': _DOMAIN + _ALLOWED_WEBHOOK,
+                        'certificate': certfile, }
+        print('try to get the: ' + _BASE_URL + _SET_WEBHOOK)
+        resp = requests.post(_BASE_URL + _SET_WEBHOOK, data=webhook_data)
+        print(resp.text)
+
+
+@app.route(_ALLOWED_WEBHOOK, methods=['POST'])
+def porcess_wh_response():
+    print(request.json)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def process_all():
+    print(request.json)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    setWebhook()
+    app.run(debug=True, port=8443)
