1bac5f7033262dde649e058de8fc066bce06f5a0
teller308 <teller308@gmail.com>
2019-08-23 22:54:01 +0400

    flask works with ssl context. webhook not working

--- src/bot.py
+++ src/bot.py
@@ -3,52 +3,61 @@ import json
 import requests
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
 
 with open('private/config.json') as config:
-    json_dict = json.load(config)
+    config_dict = json.load(config)
 
 
-_TOKEN = json_dict['token']
-_DOMAIN = json_dict['domain']
+_TOKEN = config_dict['TOKEN']
+_FQDN = config_dict['FQDN']
+_PORT = 8443
+_SSL_CONTEXT = (config_dict['SSL_CONTEXT']['CERT'],
+                config_dict['SSL_CONTEXT']['KEY'])
+_CERT_FILE = _SSL_CONTEXT[0]
 
-_BASE_URL = 'https://api.telegram.org/bot' + _TOKEN
-
+_API_URL = 'https://api.telegram.org/bot' + _TOKEN
 _GET_UPDATES = '/getUpdates'
 
 _SET_WEBHOOK = '/setWebhook'
 _DELETE_WEBHOOK = '/deleteWebhook'
 _GET_WEBHOOK_INFO = '/getWebhookInfo'
 
-_ALLOWED_WEBHOOK = '/webhook' + _TOKEN
-
 
 def setWebhook():
-    with open('private/keeper_wh_public.pem') as certfile:
-        resp = requests.get(_BASE_URL + _GET_WEBHOOK_INFO)
-        print(resp.text)
-        resp = requests.get(_BASE_URL + _DELETE_WEBHOOK)
-        print(resp.text)
+    resp = requests.get(_API_URL + _GET_WEBHOOK_INFO)
+    print(resp.text)
+    resp = requests.get(_API_URL + _DELETE_WEBHOOK)
+    print(resp.text)
 
-        webhook_data = {'url': _DOMAIN + _ALLOWED_WEBHOOK,
-                        'certificate': certfile, }
-        print('try to get the: ' + _BASE_URL + _SET_WEBHOOK)
-        resp = requests.post(_BASE_URL + _SET_WEBHOOK, data=webhook_data)
-        print(resp.text)
+    with open(_CERT_FILE) as _CERTIFICATE:
+        webhook_data = {'url': 'https://{}:{}/{}'.format(_FQDN, _PORT, _TOKEN),
+                        'certificate': _CERTIFICATE, }
+
+        print('Try POST webhook URL: {}'.format(_API_URL + _SET_WEBHOOK))
+        resp = requests.post(url=_API_URL + _SET_WEBHOOK,
+                             data=webhook_data,
+                             verify=_CERT_FILE,
+                             )
+        print('Response: {}'.format(resp.text))
 
 
-@app.route(_ALLOWED_WEBHOOK, methods=['POST'])
+@app.route('/{}'.format(_TOKEN), methods=['POST'])
 def porcess_wh_response():
     print(request.json)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def process_all():
+    print('Hello')
     print(request.json)
 
 
 if __name__ == '__main__':
-    setWebhook()
-    app.run(debug=True, port=8443)
+    # setWebhook()
+
+    app.run(host=_FQDN,
+            port=_PORT,
+            ssl_context=_SSL_CONTEXT,
+            debug=True,)
