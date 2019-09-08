3f2694401ab33e9eec290d831e1d3f935b7eb0b9
teller308 <teller308@gmail.com>
2019-09-08 19:03:56 +0400

    User auth ready. Gkeep CRUD proto ready

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 
+import requests
 from flask import request
 
 from core import application
@@ -14,34 +15,21 @@ check_webhook_response = webhookHelper.get_webhook()
 if check_webhook_response['result']['url'] == '':
     webhookHelper.set_webhook()
 
+TOKEN = application.config['TOKEN']
+API_URL = application.config['API_URL']
+
+# handler setup
+handler = GKeepHandler.get_instance()
+
 
 @application.route('/{}'.format(application.config['TOKEN']), methods=['POST'])
 def porcess_wh_response():
-    try:
-        handler = GKeepHandler.get_instance()
-        recieved_data = request.json
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug('FULL JSON RESPONCE {}'.format(recieved_data))
+    recieved_data = request.json
 
-        if recieved_data.get('message') is not None:
-            entities = recieved_data['message'].get('entities')
-            if entities is not None:
-                entity_type = entities[0].get('type')
-                if entity_type == 'bot_command':
-                    user_id = recieved_data['message']['from']['id']
-                    credentials = recieved_data['message']['text'].split(' ')
-                    if len(credentials) < 3:
-                        logger.error('Wrong arguments.')
-                        return 'Wrong arguments.'
-                    email = credentials[1]
-                    pwd = credentials[2]
-                    handler.log_in(user_id, email, pwd)
-    except Exception as exc:
-        msg = 'Something went wrong in webhook processing {}'.format(exc)
-        logger.error(msg)
-    finally:
-        # TODO remove dev mode update skipping
-        return 'Proceed pls. We do not need update duplication now.'
+    answer = handler.router(recieved_data)
+    requests.post(
+        url='{}/sendMessage'.format(API_URL), data=answer)
+    return '200'
 
 
 @application.route('/')
--- utils/gkeep_handler.py
+++ utils/gkeep_handler.py
@@ -24,74 +24,193 @@ class GKeepHandler:
     def __init__(self) -> None:
         if self._instance is None:
             self._keep_instance = gkeepapi.Keep()
-            self._active_user_emails = []
+            self._active_users_ids = []
+            self._cmd_list = {
+                'help': self._help,
+                'log_in': self._log_in,
+                'log_out': self._log_out,
+                'create': self._create_note,
+                'read': self._read_note,
+                'update': self._update_note,
+                'delete': self._delete_note,
+                'list': self._list_notes
+            }
+
+            self._required_login_cmd_list = (
+                'create', 'read', 'update', 'delete', 'list', 'log_out')
+
+            self._registered_users = GKeepUser.query.all()
 
             GKeepHandler._instance = self
 
         else:
             raise Exception('This class is a singleton.')
 
-    def _new_login(self, user_id: int, email: str, password: str) -> str:
-        # try login in gkeep service
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug('NEW GKEEP LOGIN TRY')
+    def _new_login(self, user_id: int, email: str, password: str) -> None:
         self._keep_instance.login(email, password)
-
-        # store token in keyring
         token = self._keep_instance.getMasterToken()
         keyring.set_password(self.MASTER_TOKEN_NAME, email, token)
+        self._register_new_user(user_id, email)
 
-        # to remember user
-        new_user = GKeepUser(tg_user_id=user_id, email=email)
-        db.session.add(new_user)
-        db.session.commit()
-        self._active_user_emails.append(email)
+        return 'Logged in successfully.'
 
     def _resume_login(self, user_id: int, email: str) -> None:
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug('Try resume login for: {}'.format(email))
         token = keyring.get_password(self.MASTER_TOKEN_NAME, email)
         self._keep_instance.resume(email, token)
-        self._active_user_emails.append(email)
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug('Login resumed.')
-        return token
+        self._active_users_ids.append(user_id)
+        return 'Welcome back.'
 
-    def log_in(self, user_id: int, email: str, password: str) -> str:
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug('Handler got: {} {} {}'.format(
-                user_id, email, password))
+    def _is_user_active(self, user_id: int) -> bool:
+        if user_id in self._active_users_ids:
+            return True
+        return False
+
+    def _is_user_registered(self, user_id: int) -> bool:
+        for user in self._registered_users:
+            if user.tg_user_id == user_id:
+                return True
+        return False
+
+    def _get_user_email_by_id(self, user_id: int) -> str:
+        for user in self._registered_users:
+            if user.tg_user_id == user_id:
+                return user.email
+
+    def _register_new_user(self, user_id: int, email: str) -> str:
+        new_user = GKeepUser(tg_user_id=user_id, email=email)
+        self._active_users_ids.append(user_id)
+        self._registered_users.append(new_user)
+        db.session.add(new_user)
+        db.session.commit()
+        db.session.refresh(new_user)
+        return 'Successful registration.'
+
+    def _log_in(self, *args: tuple) -> str:
+        if len(args) != 3:
+            return 'Please use this format:\n/log_in [email] [pass].'
+        email, password, user_id = args
         try:
-            if email in self._active_user_emails:
-                if logger.isEnabledFor(logging.DEBUG):
-                    logger.debug('ACTIVE USER')
-                return 'User already in system.'
-            elif GKeepUser.query.filter_by(email=email).first():
-                if logger.isEnabledFor(logging.DEBUG):
-                    logger.debug('REGISTERED USER')
-                self._resume_login(user_id, email)
+            if self._is_user_active(user_id):
+                return 'You are already in the system.'
+            elif self._is_user_registered(user_id):
+                return self._resume_login(user_id, email)
             else:
-                if logger.isEnabledFor(logging.DEBUG):
-                    logger.debug('NEW USER')
-                self._new_login(user_id, email, password)
-            return 'Success.'
+                return self._new_login(user_id, email, password)
         except Exception as exc:
             msg = 'Something went wrong when try to log in. {}'.format(exc)
             logger.error(msg)
             return 'Failed to log in.'
 
-    def log_out(self):
-        pass
+    def _log_out(self, *args: tuple) -> str:
+        user_id = args[0]
+        for user in self._registered_users:
+            if user.tg_user_id == user_id:
+                user_to_remove = user
+        self._active_users_ids.remove(user_id)
+        self._registered_users.remove(user_to_remove)
+        db.session.delete(user_to_remove)
+        db.session.commit()
 
-    def create_note(self, name: str, content: str) -> None:
-        self._keep_instance.createNote(name, content)
+        return 'Logged out.'
+
+    def _create_note(self, *args: tuple) -> str:
+        if len(args) < 3:
+            return 'Please use this format:' \
+                '\n/create [note_title] [note_content].'
+        name, content = args[0], ' '.join(args[1:-1])
+        note = self._keep_instance.createNote(name, content)
         self._keep_instance.sync()
+        return 'Created: {}'.format(note.title)
 
-    def read_note(self, name: str) -> str:
-        pass
+    def _read_note(self, *args: tuple) -> str:
+        if len(args) != 2:
+            return 'Please use this format:\n/read [note_title].'
+        name, _ = args
+        gnotes = self._keep_instance.find(query=name)
+        content = []
+        for note in gnotes:
+            if note.title != '':
+                content.append(note.title)
+                text = note.text
+                content.append(text)
+                if text[-1] != '\n':
+                    content.append('\n')
+        return ('\n').join(el for el in content)
 
-    def update_note(self) -> None:
-        pass
+    def _update_note(self, *args: tuple) -> str:
+        if len(args) != 3:
+            return 'Please use this format:' \
+                '\n/update [note_title] [new_content].'
+        name, content = args[0], ' '.join(args[1:-1])
+        gnotes = self._keep_instance.find(query=name)
+        for note in gnotes:
+            note.text = content
+        self._keep_instance.sync()
+        return 'Updated.'
 
-    def delete_note(self) -> None:
-        pass
+    def _delete_note(self, *args: tuple) -> str:
+        if len(args) != 2:
+            return 'Please use this format:\n/delete [note_title].'
+        name, _ = args
+        gnotes = self._keep_instance.find(query=name)
+        for note in gnotes:
+            note.delete()
+        self._keep_instance.sync()
+        return 'Cleared.'
+
+    def _list_notes(self, *args: tuple) -> str:
+        gnotes = self._keep_instance.all()
+        result = '\n'.join(
+            note.title for note in gnotes if note.title != '')
+        if len(result) < 4096:
+            return result
+        return 'Result is too long.'
+
+    def _help(self, *args: tuple) -> str:
+        guide_ref = 'https://support.google.com/accounts/answer/185833?' \
+            'hl=en&ctx=ch_b%2F0%2FDisplayUnlockCaptcha'
+        help_text = 'To use this bot you need to get app password. ' \
+            'Short guide here: {}'.format(guide_ref)
+        return help_text
+
+    def _is_command(self, recieved_data: dict) -> bool:
+        if recieved_data.get('message') is not None:
+            entities = recieved_data['message'].get('entities')
+            if entities is not None:
+                entity_type = entities[0].get('type')
+                if entity_type == 'bot_command':
+                    return True
+        return False
+
+    def router(self, recieved_data: dict) -> str:
+        answer_dict = {}
+        user_id = recieved_data['message']['from']['id']
+        answer_dict['chat_id'] = user_id
+
+        try:
+            if self._is_command(recieved_data) is True:
+                input_data_list = recieved_data['message']['text'].split(' ')
+                input_data_list.append(user_id)
+
+                command = input_data_list.pop(0).replace('/', '')
+
+                if command in self._required_login_cmd_list:
+                    if not self._is_user_active(user_id):
+                        if not self._is_user_registered(user_id):
+                            answer_dict['text'] = 'Please log in.'
+                            return answer_dict
+                        else:
+                            # not active but registered user
+                            email = self._get_user_email_by_id(user_id)
+                            self._resume_login(user_id, email)
+
+                func = self._cmd_list.get(command, 'Unknown command.')
+                answer_dict['text'] = func(*input_data_list)
+            else:
+                answer_dict['text'] = 'Please use a command from the list.'
+            return answer_dict
+        except Exception as exc:
+            msg = 'Something went wrong in message processing {}'.format(exc)
+            logger.error(msg)
+            answer_dict['text'] = 'Something went wrong in message processing.'
+            return answer_dict
